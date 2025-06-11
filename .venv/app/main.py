72bf6977021e837a0a8d8ff2173957741366cabc
Salterm27 <sebastian.alterman@gmail.com>
2025-06-11 10:16:26 -0300

    Integrated `predict` endpoint and updated workspace.xml with recent file path and task changes.

--- .venv/app/main.py
+++ .venv/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import location, enrichment
+from app.routes import location, enrichment, predict
 
 from dotenv import load_dotenv
 import os
@@ -11,9 +11,11 @@ OPENAI_API_KEY = os.getenv("openai_key")
 
 app = FastAPI()
 
-app.include_router(location.router)
-app.include_router(enrichment.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+app.include_router(predict.router)
+app.include_router(location.router)
+app.include_router(enrichment.router)
