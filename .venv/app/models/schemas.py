31cf6cf61135cc7db9e0adbab6492ad11a867018
Salterm27 <sebastian.alterman@gmail.com>
2025-06-11 10:15:07 -0300

    Adding the schemas for the functions to run, and the init for each repo

---
+++ .venv/app/models/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Dict
+
+class Coordinates(BaseModel):
+    latitude: float
+    longitude: float
+
+class TagRequest(BaseModel):
+    city: str
+    category: str
+
+class AttributeRequest(BaseModel):
+    city: str
+    postal_code: str
+    num_competitors: int
+    street_type: Dict[str, int]
+    tags: Dict[str, int]
+
+class   Prediction_Input(BaseModel):
+    latitude: float
+    longitude: float
+    category: str
+    n_competitors_1km: int
